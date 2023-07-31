012d4fac01daf49fcab0e12ed22d66cc7494c9a8
Rob Fraley <robertdfraley@hotmail.com>
2023-07-31 19:30:20 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 # Write a program that outputs the numbers from 1 to 100. All numbers that are divisible by three should be replaced by 
 # Fizz and all numbers that are divisible by 5 should be replaced by Buzz. Numbers that are divisible by both 3 and 5 
-# will be replaced by "FizzBuzz".
+# will be replaced by "FizzBuzz". 
 
 for i in range(1,101):
     if i % 3 == 0 and i % 5 == 0:
